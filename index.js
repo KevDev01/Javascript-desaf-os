27b7a9127057a54f5964acc139e2fe338ca9c449
Kevin Carrasquedo <kevincarrasquedo@gmail.com>
2022-09-20 23:02:35 -0500

    DOMv1

--- index.js
+++ index.js
@@ -1,42 +1,133 @@
-
-class Producto{
-    constructor(nombre, precio, cantidad){
+class Producto {
+    constructor(nombre, precio) {
         this.nombre = nombre;
         this.precio = parseFloat(precio);
-        this.cantidad = cantidad;
     }
-
-    totalIva() {
-        return (this.precio * 1.16) * this.cantidad;
-    }
-
 }
 
 var arrayProductos = [];
-do{
-    var comprobacion = prompt("Ingresa un nombre de producto o esc para terminar");
-    if (comprobacion === "esc"|| comprobacion === "ESC"|| comprobacion === "Esc" ){
-        break;
-    }else{
-    var nombreP = comprobacion;
-    var precioP = parseInt(prompt("Ingrese el precio del producto (Solo números)"));
-    var cantidadP = parseInt(prompt("Ingrese la cantidad deseada (Solo números)"));
-    arrayProductos.push(new Producto(nombreP, precioP, cantidadP));
-    }
-} 
-while (comprobacion != "esc"|| comprobacion != "ESC"|| comprobacion != "Esc" )
+var arrayCantidad = [];
+for (var i = 1; i < 99; i++) {
+    arrayCantidad.push(i);
+}
+console.log(arrayCantidad)
+
+function agregarProducto() {
+    var nombreP = document.getElementById('nombre').value;
+    var precioP = document.getElementById('precio').value;
+    arrayProductos.push(new Producto(nombreP, precioP));
+}
 
 console.log(arrayProductos)
 
-for (var producto of arrayProductos){
-    document.write("<h3> El producto ingresado es : " + producto.nombre + "</h3>");
-    document.write("<h3> El precio sin IVA del producto ingresado es : $" + producto.precio + "</h3>");
-    document.write("<h3> La cantidad deseada es : " + producto.cantidad + "</h3>");
-    document.write("<h3> El total de los productos con IVA es : $" + producto.totalIva() + "</h3>");
-    document.write("----------------------------------------------------");
+function mostrarProductos() {
+    var resultado = document.getElementById("resultado");
 
-    console.log(producto.nombre);
-    console.log(producto.precio);
-    console.log(producto.cantidad);
-    console.log(producto.totalIva);
+    for (const producto of arrayProductos) {
+        var div = document.createElement("div");
+        div.innerHTML = `
+    <h3> Nombre del producto: ${producto.nombre}</h3> 
+    <p> Precio: $${producto.precio}</p> 
+    `;
+        resultado.append(div);
+    }
+
+    var selectProducto = document.getElementById("eligeProducto");
+    for (const opciones of arrayProductos) {
+        var opt = opciones.nombre;
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        selectProducto.appendChild(el);
+    }
+
+    var selectCantidad = document.getElementById("eligeCantidad")
+    for (const cant of arrayCantidad) {
+        var cantidad = cant;
+        var num = document.createElement("option");
+        num.textContent = cantidad;
+        num.value = cantidad;
+        selectCantidad.appendChild(num);
+    }
+
+}
+var iterator; 
+iterator=1;
+function añadirOtro() {
+        var dupproducto;
+        var dupcantidad;
+        window['dupproducto'+iterator] = document.createElement('select');
+        window['dupproducto'+iterator].id = 'productodup'+iterator;
+        window['dupcantidad'+iterator] = document.createElement('select');
+        window['dupcantidad'+iterator].id = 'cantidaddup'+iterator;
+
+        for (const opciones of arrayProductos) {
+            var opt = opciones.nombre;
+            var el = document.createElement("option");
+            el.textContent = opt;
+            el.value = opt;
+            window['dupproducto'+iterator].appendChild(el);
+        }
+        for (const cant of arrayCantidad) {
+            var cantidad = cant;
+            var num = document.createElement("option");
+            num.textContent = cantidad;
+            num.value = cantidad;
+            window['dupcantidad'+iterator].appendChild(num);
+        }
+        //add the element to the form
+        document.getElementById("prueba").appendChild(window['dupproducto'+iterator]);
+        document.getElementById("prueba").appendChild(window['dupcantidad'+iterator]);
+        iterator++;
+}
+
+function cotizar() {
+    
+    var outputProducto = document.getElementById("eligeProducto").value;
+    var outputCantidad = document.getElementById("eligeCantidad").value;
+    
+    var cotizacion = document.getElementById("cotizacion");
+    for (let i = 0; i < arrayProductos.length; i++) {
+        var producto = arrayProductos[i];
+        
+        if (outputProducto === producto.nombre) {
+            var precio = producto.precio;
+            var subtotal1 = precio * outputCantidad;
+        }
+          
+    }
+    var div = document.createElement ("div");
+    div.innerHTML = `
+    <p> ${outputProducto}   $${precio}   ${outputCantidad}</p>;
+    `
+    cotizacion.append(div);
+    var subtotal2=0;
+
+    for (x=1;x<iterator;x++){
+    var ImpresionProd = document.getElementById('productodup'+x).value;
+    var ImpresionCant = document.getElementById('cantidaddup'+x).value;
+        var producto = arrayProductos[x];
+        
+        if (ImpresionProd === producto.nombre) {
+            var precio1 = producto.precio
+            subtotal2 += precio1 * ImpresionCant;
+            
+        }
+
+    
+    var div = document.createElement ("div");
+    div.innerHTML = `
+    <p> ${ImpresionProd}   $${precio1}   ${ImpresionCant}</p>;
+    `
+    cotizacion.append(div);  
+}
+var total = (subtotal1 + subtotal2) * 1.16;
+console.log("El total con IVA es: "+total);
+var div = document.createElement ("div");
+    div.innerHTML = `
+    <p> El total con Iva es: $${total}</p>;
+    `
+    cotizacion.append(div);  
+//console.log(subtotal1);
+   // console.log(subtotal2);
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+<h1>Cotizador</h1>
+
+<p>Este es un sistema que te permite crear una cotización</p>
